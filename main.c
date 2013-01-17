e9b7cd96acc6b52ea442c30fc0517d4e9f3abcb6
Colin-N. Brosseau <colin.brosseau@gmail.com>
2013-01-16 20:45:41 -0500

    code cleaning

--- main.c
+++ main.c
@@ -25,8 +25,8 @@ unsigned char data_in[8];
 char command_in[8];
 unsigned char command_ready = FALSE;
 //const unsigned char command[] ="output:";
-unsigned int sensitivity = 23;
-unsigned int W = 12;
+int variable_A = 23;
+int variable_goto = 12;
 
 char command_name[10];
 
@@ -37,46 +37,45 @@ void copy_command(void);
 void process_command(void);
 void print_value (char *id, int value);
 void uart_ok(void);
-unsigned long parse_assignment (char input[16]);
+int parse_assignment (char input[16]);
 
-unsigned long parse_assignment (char input[16])
+int parse_assignment (char input[16])
 {
-    char *pch;
-    char cmdValue[16];
-    // Find the position the equals sign is
-    // in the string, keep a pointer to it
-    pch = strchr(command_in, '=');
-    // Copy everything after that point into
-    // the buffer variable
-    strcpy(cmdValue, pch+1);
-    // Now turn this value into an integer and
-	// return it to the caller.
-    return atoi(cmdValue);
+  char *pch;
+  char cmdValue[16];
+  // Find the position the equals sign is
+  // in the string, keep a pointer to it
+  pch = strchr(input, '=');
+  // Copy everything after that point into
+  // the buffer variable
+  strcpy(cmdValue, pch+1);
+  // Now turn this value into an integer and
+  // return it to the caller.
+  return atoi(cmdValue);
 }
-
 
 void copy_command ()
 {
-        // Copy the contents of data_in into command_in
-        memcpy(command_in, data_in, 8);
-        // Now clear data_in, the UART can reuse it now
-        memset(data_in, 0, 8);
+  // Copy the contents of data_in into command_in
+  memcpy(command_in, data_in, 8);
+  // Now clear data_in, the UART can reuse it now
+  memset(data_in, 0, 8);
 }
 
 void process_command()
 {
   if(strcasestr(command_in,"GOTO") != NULL){
     if(strcasestr(command_in,"?") != NULL)
-      print_value("goto", W);
+      print_value("goto", variable_goto);
     else
-      W = parse_assignment(command_in);
+      variable_goto = parse_assignment(command_in);
   }
   else if(strcasestr(command_in,"A") != NULL){
     if(strcasestr(command_in,"?") != NULL)
-      print_value("A  ", sensitivity);
+      print_value("A", variable_A);
     else
-      sensitivity = parse_assignment(command_in);     
-    };
+      variable_A = parse_assignment(command_in);
+  }
 } 
 
 void print_value (char *id, int value)
@@ -98,8 +97,6 @@ void uart_ok()
 int main(void)
 {
   unsigned int c;
-  char buffer[7];
-  int  num=134;
     
   /*
    *  Initialize UART library, pass baudrate and AVR cpu clock
@@ -122,32 +119,13 @@ int main(void)
    *  uart_puts() blocks if it can not write the whole string to the circular 
    *  buffer
    */
-  uart_puts("----- RETURN_NEWLINE");
-  uart_puts("AVR U1000 RETURN_NEWLINE");
-  
-  /*
-   * Transmit string from program memory to UART
-   */
-  uart_puts("Version 1.9 RETURN_NEWLINE");
-  
-  
-  /* 
-   * Use standard avr-libc functions to convert numbers into string
-   * before transmitting via UART
-   */     
-  itoa( num, buffer, 10);   // convert interger into string (decimal format)         
-  uart_puts(buffer);        // and transmit string to UART
-  uart_puts(" RETURN_NEWLINE");
-  
-  
-  /*
-   * Transmit single character to UART
-   */
-  uart_putc('\r');
-  
-  uart_puts_P("patate1 RETURN_NEWLINE");
-
-  //    for(;;)
+  uart_puts("-----");
+  uart_puts(RETURN_NEWLINE);
+  uart_puts("AVR UART");
+  uart_puts(RETURN_NEWLINE);
+  uart_puts("Command parsing demo");
+  uart_puts(RETURN_NEWLINE);
+ 
   while (1)
     {
       /*
@@ -161,16 +139,11 @@ int main(void)
 
       if ( c & UART_NO_DATA )
         {
-	  /* 
-	   * no data available from UART 
-	   */
+	  // no data available from UART 
         }
       else
 	{
-	  /*
-	   * new data available from UART
-	   * check for Frame or Overrun error
-	   */
+	  // new data available from UART check for Frame or Overrun error
 	  if ( c & UART_FRAME_ERROR )
 	      {
                 /* Framing Error detected, i.e no stop bit detected */
@@ -178,45 +151,42 @@ int main(void)
 	      }
 	  if ( c & UART_OVERRUN_ERROR )
             {
-	      /* 
-	       * Overrun, a character already present in the UART UDR register was 
+	       /* Overrun, a character already present in the UART UDR register was 
 	       * not read by the interrupt handler before the next character arrived,
-	       * one or more received characters have been dropped
-	       */
+	       * one or more received characters have been dropped */
 	      uart_puts_P("UART Overrun Error: ");
             }
 	  if ( c & UART_BUFFER_OVERFLOW )
             {
-                /* 
-                 * We are not reading the receive buffer fast enough,
-                 * one or more received character have been dropped 
-                 */
+                 /* We are not reading the receive buffer fast enough,
+		  * one or more received character have been dropped  */
                 uart_puts_P("Buffer overflow error: ");
             }
-	  /* 
-	   * send received character back
-	   */
 
-	  // Get data from the UART in register
+	  // Add char to input buffer
 	  data_in[data_count] = c;
 	  
-	  // End of line!
-	  if (data_in[data_count] == '\r') {
-	    command_ready = TRUE;
+	  // Return is signal for end of command input
+	  if (data_in[data_count] == CHAR_RETURN) {
+	    //command_ready = TRUE;
 	    // Reset to 0, ready to go again
 	    data_count = 0;
 	    uart_puts(RETURN_NEWLINE);
+
+	    copy_command();
+	    process_command();
+	    uart_ok();
 	  } else {
 	    data_count++;
 	  }
 
-	  if (command_ready == TRUE) {
-	    copy_command();
-            process_command();
+	  /* if (command_ready == TRUE) { */
+	  /*   copy_command(); */
+          /*   process_command(); */
 	    
-            command_ready = FALSE;
-            uart_ok();
-	  }
+          /*   command_ready = FALSE; */
+          /*   uart_ok(); */
+	  /* } */
 	  
 	  uart_putc( (unsigned char)c );
 	  
